a534016581138667bd0fcf975d0b789df0843327
Anton Pusch <github@anton-pusch.de>
2024-08-17 04:02:37 +0200

    Float thumbnail in the top left corner next to the installation instruction

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Anton's🪴",
+    pageTitle: "Anton's Digital Garden",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
