77b52c3ed8e7365911d2f204e97a30d620294df2
Anton Pusch <github@anton-pusch.de>
2024-08-17 04:15:54 +0200

    Put backlinks on left column on desktop

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -27,12 +27,13 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
+    Component.DesktopOnly(Component.Backlinks()),
     // Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
     // Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
+    Component.MobileOnly(Component.Backlinks()),
   ],
 }
 
