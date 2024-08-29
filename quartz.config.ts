bf592d51e88c2b3fadb6490e3b21d04366a4c2e0
Anton Pusch <github@anton-pusch.de>
2024-08-29 10:11:20 +0200

    update base url

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.antonpusch.de",
+    baseUrl: "antonpusch.de",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
